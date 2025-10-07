e6c5a4872774c764fb9409c5ace558dedf596b61
Shardul Joshi <sharduljoshi2001@gmail.com>
2025-10-07 15:08:08 +0530

    issues fixed again

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import {defineConfig} from "vite"
 
 export default defineConfig({
-	plugins: [
-		
-	]
+	plugins: [],
+	build: {
+		outDir: 'dist',
+		assetsDir: 'assets',
+		sourcemap: false,
+		minify: 'terser',
+		rollupOptions: {
+			output: {
+				entryFileNames: 'assets/[name].[hash].js',
+				chunkFileNames: 'assets/[name].[hash].js',
+				assetFileNames: 'assets/[name].[hash].[ext]'
+			}
+		}
+	},
+	base: './',
+	server: {
+		port: 3000,
+		open: true
+	}
 })
